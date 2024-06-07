228dc21bc3896ec24a003c1d4dc14112e4379e43
DoStr1 <d9787333978@gmail.com>
2024-06-08 00:21:05 +0300

    Final Version

--- Task.py
+++ Task.py
@@ -1,7 +1,29 @@
 def to_binary(num, store):
-    bin_num = str(bin(num)[2::])
-    s = bin_num.zfill(store)
+    bin_num = bin(num)[2:]
+    if len(bin_num) >= store:
+        print("Need More Space")
+        return
+    
+    bin_num = bin_num.zfill(store)
+    
     translation_table = str.maketrans("01", "10")
-    s = s.translate(translation_table)
+    ones_complement = bin_num.translate(translation_table)
+    twos_complement = list(ones_complement)
+    
+    carry = 1
+    for i in range(len(twos_complement) - 1, -1, -1):
+        if carry == 1:
+            if twos_complement[i] == '1':
+                twos_complement[i] = '0'
+            else:
+                twos_complement[i] = '1'
+                carry = 0
+
+    twos_complement_str = ''.join(twos_complement)
+    result = '1' + twos_complement_str[1:]  
+    
+    print(result)
+
+
 
 to_binary(9,8)
