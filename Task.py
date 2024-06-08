ef9e0d491e6d5b445ba578b9b79940f0d7f8d761
DoStr1 <d9787333978@gmail.com>
2024-06-08 10:41:30 +0300

    More Efficient version

--- Task.py
+++ Task.py
@@ -6,18 +6,18 @@ def to_binary(num, store):
     
     bin_num = bin_num.zfill(store)
     
-    translation_table = str.maketrans("01", "10")
-    ones_complement = bin_num.translate(translation_table)
+    ones_complement = ''.join('1' if bit == '0' else '0' for bit in bin_num)
     twos_complement = list(ones_complement)
     
     carry = 1
     for i in range(len(twos_complement) - 1, -1, -1):
-        if carry == 1:
-            if twos_complement[i] == '1':
-                twos_complement[i] = '0'
-            else:
-                twos_complement[i] = '1'
-                carry = 0
+        if carry == 0:
+            break
+        if twos_complement[i] == '1':
+            twos_complement[i] = '0'
+        else:
+            twos_complement[i] = '1'
+            carry = 0
 
     twos_complement_str = ''.join(twos_complement)
     result = '1' + twos_complement_str[1:]  
@@ -26,4 +26,4 @@ def to_binary(num, store):
 
 
 
-to_binary(9,8)
+to_binary(42,8)
